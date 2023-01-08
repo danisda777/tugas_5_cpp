d65a1248260179b79987ac5faf6b08e959c07cd2
Sahrul Dani Ardiansyah <daniardiansyah448@gmail.com>
2023-01-08 10:10:53 +0700

    Tugas 5 C++
    Dikumpulkan kepada Niomic

---
+++ Tugas5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main (){
+	int x,y;
+	cout<<"Inputkan Nilai Yang Ingin Dipangkatkan : ";
+	cin>>x;
+	cout<<"Inputkan Nilai Pangkat : "; 
+	cin>>y;
+	x=pow(x,y);
+	cout<<"Hasil Perpangkatan : "<<x;
+	
+}
